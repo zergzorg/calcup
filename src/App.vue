50f8b927c0ced2fd78029afb772d86503d3539e3
Senokosov.AI <Senokosov.AI@gazprom-neft.ru>
2025-02-26 15:21:22 +0300

    Добавлены часы в центр экрана

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { ref, onMounted } from "vue";
+
+const time = ref(new Date().toLocaleTimeString());
+
+onMounted(() => {
+  setInterval(() => {
+    time.value = new Date().toLocaleTimeString();
+  }, 1000);
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="clock-container">
+    <h1>{{ time }}</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.clock-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-size: 5rem;
+  font-family: Arial, sans-serif;
 }
 </style>
