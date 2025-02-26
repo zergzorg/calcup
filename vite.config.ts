cdc092ee25ab6318e148cfac2fb8c89ed161ef82
Senokosov.AI <Senokosov.AI@gazprom-neft.ru>
2025-02-26 16:11:22 +0300

    Restore changes after pull and rebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  base: "./",  // Относительный путь, чтобы файлы были в корне
+});
